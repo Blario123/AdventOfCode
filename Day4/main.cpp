8350c7c5bfe75bad68287d3e7634ed4da84d647b
Blair Liversidge <19518532+Blario123@users.noreply.github.com>
2022-12-14 22:49:13 +0000

    Day 4: Part 2

--- Day4/main.cpp
+++ Day4/main.cpp
@@ -7,18 +7,28 @@ struct section{
 };
 
 bool isContained(section elf1, section elf2, bool r = true) {
-//    std::cout << "Elf 1: Min: " << elf1.min << " Max: " << elf1.max << " Elf 2: Min: " << elf2.min << " Max: " << elf2.max << std::endl;
     // Run first pass with elf1 and elf2, return true if it is contained within.
     if(elf1.min >= elf2.min && elf1.max <= elf2.max) {
-//        std::cout << "Contained" << std::endl;
         return true;
     }
     // If elf2 does not contain elf1, swap them around, but avoid recursion for successive fails.
     if(r) {
-//        std::cout << "Attempt with elf2 and elf1 swapped" << std::endl;
         return isContained(elf2, elf1, false);
     }
     // No match for elf1:elf2 or elf2:elf1, return false.
+    return false;
+}
+
+bool isOverlapped(section elf1, section elf2, bool r = true) {
+    if(elf1.min <= elf2.max && elf1.min >= elf2.min) {
+        return true;
+    }
+    if(elf1.max >= elf2.min && elf1.max <= elf2.max) {
+        return true;
+    }
+    if(r) {
+        return (isOverlapped(elf2, elf1, false));
+    }
     return false;
 }
 
@@ -37,16 +47,23 @@ int main(int argc, char *argv[]) {
         return -1;
     }
     std::string temp;
-    int count;
+    int containCount;
+    int overlapCount;
     while(getline(file, temp)) {
         std::string elf1Section = temp.substr(0, temp.find(','));
         std::string elf2Section = temp.substr(temp.find(',') + 1, temp.size());
-        if(isContained(createSection(elf1Section), createSection(elf2Section))) {
-            count++;
+        section elf1 = createSection(elf1Section);
+        section elf2 = createSection(elf2Section);
+        if(isContained(elf1, elf2)) {
+            containCount++;
+        }
+        if(isOverlapped(elf1, elf2)) {
+            overlapCount++;
         }
     }
     
-    std::cout << "Contained pair count: " << count << std::endl;
-
+    std::cout << "Contained pair count: " << containCount << std::endl;
+    std::cout << "Overlapping pair count: " << overlapCount << std::endl;
+    
     return 0;
 }
