a03be82a7e81482a833e6b8dc7d7dfca1a67f940
Blair Liversidge <19518532+Blario123@users.noreply.github.com>
2023-01-03 16:55:20 +0000

    Day 7: Part 1 -- INCOMPLETE
    Added hashing to dir and removed redundant code from file struct

--- Day7/main.cpp
+++ Day7/main.cpp
@@ -8,10 +8,10 @@ std::ifstream input;
 struct file;
 struct dir;
 
-dir *tempDir;
-dir *prevDir;
-
 std::vector<dir> directories;
+
+dir *tempDir = nullptr;
+dir *prevDir = nullptr;
 
 void processLine(const std::string&);
 file processItem(const std::string&, dir &);
@@ -21,39 +21,54 @@ int main(int, char**);
 struct file {
     long size = 0;
     std::string name;
-    bool operator==(const file &rhs) const {
-        return this->size == rhs.size && this->name == rhs.name;
-    }
 };
 
 struct dir {
     dir *parent = nullptr;
     std::vector<file> files;
-    std::vector<std::string> subdirs;
-    std::vector<dir> subDirs;
+    std::vector<dir*> subDirs;
+    unsigned long hash;
     std::string name;
-    long size() {
+    long size;
+    void processSize() {
         long s = 0;
-        for(auto i: files) {
+        for(const auto& i: files) {
             s += i.size;
         }
-        return s;
+        size = s;
+    }
+    void processHash(long i) {
+        // The hash is calculated from the name, parent name if available,
+        // the size, and the index of the directory within directories.
+        // The index is used to remove duplicates as some dirs,
+        // have the same name, size and parent.
+        auto nameHash = std::hash<std::string>{}(name);
+        auto sizeHash = std::hash<long>{}(size);
+        unsigned long finalHash;
+        if(parent != nullptr) {
+            auto parentHash = std::hash<std::string>{}(parent->name);
+            finalHash = std::hash<std::size_t>{}(nameHash ^ (parentHash << 1) ^ (sizeHash << 1) ^ i);
+        } else {
+            finalHash = std::hash<std::size_t>{}(nameHash & (sizeHash << 1) ^ i);
+        }
+        hash = finalHash;
     }
     void processSubdirs() {
-        std::cout << name << ".processSubdirs()" << std::endl;
+//        std::cout << name << ".processSubdirs()" << std::endl;
         for(auto &i: directories) {
-            if(i.name != "/") {
-                if(i.parent->name == name) {
-                    std::cout << i.name << ".parent = " << (std::string) i.parent->name << std::endl;
-                    subDirs.push_back(i);
-                }
+            if(i.name == "/") {
+                continue;
+            }
+            if(i.parent->hash == hash) {
+//                std::cout << i.name << ".parent = " << (std::string) i.parent->name << std::endl;
+                subDirs.push_back(&i);
             }
         }
     }
 };
 
 file processItem(const std::string &s, dir &d) {
-    std::cout << s << std::endl;
+//    std::cout << s << std::endl;
     file tempFile;
     int spacePos;
     spacePos = s.find_first_of(" ");
@@ -61,8 +76,6 @@ file processItem(const std::string &s, dir &d) {
         tempFile.size = (long) std::stoi(s.substr(0, spacePos++));
         tempFile.name = s.substr(spacePos, s.size() - spacePos);
     } else {
-        std::string name = s.substr(++spacePos, s.size() - spacePos);
-        d.subdirs.emplace_back(name);
         tempFile.size = -1;
     }
     return tempFile;
@@ -73,7 +86,7 @@ void processLine(const std::string &s) {
     // Process them accordingly.
     if(s.find("$ ls") != std::string::npos) {
         long index;
-        std::cout << s << std::endl;
+//        std::cout << s << std::endl;
         std::string temp;
         for(dir const &i: directories) {
             if(i.name == tempDir->name) {
@@ -93,7 +106,7 @@ void processLine(const std::string &s) {
             }
         }
     } else if(s.find("$ cd") != std::string::npos) {
-        std::cout << s << std::endl;
+//        std::cout << s << std::endl;
         std::string n = s.substr(5, s.size() - 5);
         if(n == "..") {
             tempDir = (dir*) tempDir->parent;
@@ -112,14 +125,16 @@ void processLine(const std::string &s) {
 }
 
 long calculateTotal(dir &d) {
+//    std::cout << d.name << " calculateTotal" << std::endl;
     long total = 0;
-    if(d.size() <= 100000) {
-        total += d.size();
+    if(d.size <= 100000) {
+        total += d.size;
     } else {
         return -1;
     }
-    for(auto &i: d.subDirs) {
-        long t = calculateTotal(i);
+    for(auto i: d.subDirs) {
+//        std::cout << i->subDirs.size() << std::endl;
+        long t = calculateTotal(*i);
         if(t <= 100000) {
             total += t;
         }
@@ -145,7 +160,12 @@ int main(int argc, char *argv[]) {
     while(getline(input, temp)) {
         processLine(temp);                
     }
-
+    for(auto &i: directories) {
+        i.processSize();
+        long index;
+        index = std::addressof(i) - std::addressof(directories[0]);
+        i.processHash(index);
+    }
     for(auto &i: directories) {
         i.processSubdirs();
     }
@@ -161,3 +181,6 @@ int main(int argc, char *argv[]) {
     std::cout << std::endl << "Total = " << total << std::endl;
     return 0;
 }
+
+// Is not higher than 1869999
+// Is not lower than 1069572
