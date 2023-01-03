316e970a3f2e2e8537a8d0d857dda5a8208f743c
Blair Liversidge <19518532+Blario123@users.noreply.github.com>
2023-01-03 22:55:59 +0000

    Day 7: Part 1 -- INCOMPLETE
    One step kind of forwards, three steps backwards
    
    Hashing of directories can be completed when the directory is created, however it needs to wait until the files have been iterated

--- Day7/main.cpp
+++ Day7/main.cpp
@@ -10,28 +10,30 @@ struct dir;
 
 std::vector<dir> directories;
 
-dir *tempDir = nullptr;
-dir *prevDir = nullptr;
+dir *tempDir;
+dir *prevDir;
+std::size_t index = 0;
+int count = 0;
 
 void processLine(const std::string&);
-file processItem(const std::string&, dir &);
-long calculateTotal(dir &);
+file processItem(const std::string&);
+unsigned long calculateTotal(dir &, int);
 int main(int, char**);
 
 struct file {
-    long size = 0;
+    unsigned long size = 0;
     std::string name;
 };
 
 struct dir {
-    dir *parent = nullptr;
-    std::vector<file> files;
-    std::vector<dir*> subDirs;
-    unsigned long hash;
     std::string name;
-    long size;
+    dir *parent = nullptr;
+    unsigned long size = 0;
+    unsigned long hash = 0;
+    std::vector<file> files;
+    std::vector<dir> subDirs;
     void processSize() {
-        long s = 0;
+        unsigned long s = 0;
         for(const auto& i: files) {
             s += i.size;
         }
@@ -43,35 +45,31 @@ struct dir {
         // The index is used to remove duplicates as some dirs,
         // have the same name, size and parent.
         auto nameHash = std::hash<std::string>{}(name);
-        auto sizeHash = std::hash<long>{}(size);
-        unsigned long finalHash;
+        auto sizeHash = std::hash<unsigned long>{}(size);
         if(parent != nullptr) {
             auto parentHash = std::hash<std::string>{}(parent->name);
-            finalHash = std::hash<std::size_t>{}(nameHash ^ (parentHash << 1) ^ (sizeHash << 1) ^ i);
+            hash = std::hash<std::size_t>{}(nameHash ^ (parentHash << 1) ^ (sizeHash << 1) ^ i);
         } else {
-            finalHash = std::hash<std::size_t>{}(nameHash & (sizeHash << 1) ^ i);
+            hash = std::hash<std::size_t>{}(nameHash & (sizeHash << 1) ^ i);
         }
-        hash = finalHash;
     }
     void processSubdirs() {
-//        std::cout << name << ".processSubdirs()" << std::endl;
-        for(auto &i: directories) {
-            if(i.name == "/") {
-                continue;
-            }
-            if(i.parent->hash == hash) {
-//                std::cout << i.name << ".parent = " << (std::string) i.parent->name << std::endl;
-                subDirs.push_back(&i);
+        for(auto i: directories) {
+            if(i.name != "/") {
+                if(i.parent->hash == hash) {
+                    std::cout << i.name << ".parent = " << (std::string) i.parent->name << std::endl;
+                    subDirs.push_back(i);
+                }
             }
         }
     }
 };
 
-file processItem(const std::string &s, dir &d) {
+file processItem(const std::string &s) {
 //    std::cout << s << std::endl;
     file tempFile;
-    int spacePos;
-    spacePos = s.find_first_of(" ");
+    unsigned long spacePos;
+    spacePos = s.find_first_of(' ');
     if(s.find("dir") == std::string::npos) {
         tempFile.size = (long) std::stoi(s.substr(0, spacePos++));
         tempFile.name = s.substr(spacePos, s.size() - spacePos);
@@ -84,32 +82,32 @@ file processItem(const std::string &s, dir &d) {
 void processLine(const std::string &s) {
     // Two possible commands; ls and cd.
     // Process them accordingly.
+    std::string n;
     if(s.find("$ ls") != std::string::npos) {
-        long index;
 //        std::cout << s << std::endl;
         std::string temp;
-        for(dir const &i: directories) {
-            if(i.name == tempDir->name) {
-                index = std::addressof(i) - std::addressof(directories[0]);
-                break;
-            }
-        }
         while(getline(input, temp)) {
             if(temp.find("$ cd") != std::string::npos) {
                 processLine(temp);
                 break;
             } else {
-                file toAdd = processItem(temp, directories[index]);
+                file toAdd = processItem(temp);
                 if(toAdd.size != -1) {
-                    directories[index].files.emplace_back(toAdd);
+                    tempDir->files.emplace_back(toAdd);
                 }
             }
         }
     } else if(s.find("$ cd") != std::string::npos) {
+        if(tempDir != nullptr) {
+            // Finished processing files to be added
+            tempDir->processSize();
+            tempDir->processHash(count++);
+            directories.at(index) = *tempDir;
+        }
 //        std::cout << s << std::endl;
-        std::string n = s.substr(5, s.size() - 5);
+        n = s.substr(5, s.size() - 5);
         if(n == "..") {
-            tempDir = (dir*) tempDir->parent;
+            tempDir = tempDir->parent;
         } else {
             if(tempDir != nullptr) {
                 prevDir = tempDir;
@@ -119,22 +117,24 @@ void processLine(const std::string &s) {
             if(n != "/") {
                 tempDir->parent = prevDir;
             }
-            directories.push_back(*tempDir);
+            directories.emplace_back(*tempDir);
+            index = directories.size() - 1;
         }
     }
 }
 
-long calculateTotal(dir &d) {
-//    std::cout << d.name << " calculateTotal" << std::endl;
-    long total = 0;
+unsigned long calculateTotal(dir &d, int depth) {
+//    std::cout << d.name << " calculateTotal, depth = " << depth << std::endl;
+    unsigned long total = 0;
     if(d.size <= 100000) {
         total += d.size;
     } else {
         return -1;
     }
-    for(auto i: d.subDirs) {
+//    std::cout << d.name << " subDirs.size = " << d.subDirs.size() << std::endl;
+    for(auto &i: d.subDirs) {
 //        std::cout << i->subDirs.size() << std::endl;
-        long t = calculateTotal(*i);
+        unsigned long t = calculateTotal(i, ++depth);
         if(t <= 100000) {
             total += t;
         }
@@ -161,23 +161,12 @@ int main(int argc, char *argv[]) {
         processLine(temp);                
     }
     for(auto &i: directories) {
-        i.processSize();
-        long index;
-        index = std::addressof(i) - std::addressof(directories[0]);
-        i.processHash(index);
-    }
-    for(auto &i: directories) {
         i.processSubdirs();
     }
-
-    long total = 0;
-    for(auto &i: directories) {
-        long tempL = calculateTotal(i);
-        if(tempL > 0) {
-            total += tempL;
-        }
+    unsigned long total = 0;
+    for(auto i: directories) {
+        total += calculateTotal(i, 0);
     }
-
     std::cout << std::endl << "Total = " << total << std::endl;
     return 0;
 }
