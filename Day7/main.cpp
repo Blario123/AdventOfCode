8aa1d177ea3dd76c05231b288e575d9b3d55889b
Blair Liversidge <19518532+Blario123@users.noreply.github.com>
2023-01-06 12:12:28 +0000

    Day 7: Part 1 -- INCOMPLETE
    Rework the dir vector construction to work from a vector of strings instead of reading the lines one by one from the file

--- Day7/main.cpp
+++ Day7/main.cpp
@@ -1,24 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <list>
 
 std::ifstream input;
 
 struct file;
 struct dir;
-
-std::vector<dir> directories;
-
-dir *tempDir;
-dir *prevDir;
-std::size_t index = 0;
-int count = 0;
-
-void processLine(const std::string&);
-file processItem(const std::string&);
-unsigned long calculateTotal(dir &, int);
-int main(int, char**);
 
 struct file {
     unsigned long size = 0;
@@ -29,49 +16,17 @@ struct dir {
     std::string name;
     dir *parent = nullptr;
     unsigned long size = 0;
-    unsigned long hash = 0;
+    unsigned long hash;
+    int fileLine = 0;
     std::vector<file> files;
     std::vector<dir> subDirs;
-    void processSize() {
-        unsigned long s = 0;
-        for(const auto& i: files) {
-            s += i.size;
-        }
-        size = s;
-    }
-    void processHash(long i) {
-        // The hash is calculated from the name, parent name if available,
-        // the size, and the index of the directory within directories.
-        // The index is used to remove duplicates as some dirs,
-        // have the same name, size and parent.
-        auto nameHash = std::hash<std::string>{}(name);
-        auto sizeHash = std::hash<unsigned long>{}(size);
-        if(parent != nullptr) {
-            auto parentHash = std::hash<std::string>{}(parent->name);
-            hash = std::hash<std::size_t>{}(nameHash ^ (parentHash << 1) ^ (sizeHash << 1) ^ i);
-        } else {
-            hash = std::hash<std::size_t>{}(nameHash & (sizeHash << 1) ^ i);
-        }
-    }
-    void processSubdirs() {
-        for(auto i: directories) {
-            if(i.name != "/") {
-                if(i.parent->hash == hash) {
-                    std::cout << i.name << ".parent = " << (std::string) i.parent->name << std::endl;
-                    subDirs.push_back(i);
-                }
-            }
-        }
-    }
 };
 
-file processItem(const std::string &s) {
-//    std::cout << s << std::endl;
+file createFile(const std::string &s) {
     file tempFile;
-    unsigned long spacePos;
-    spacePos = s.find_first_of(' ');
+    unsigned long spacePos = s.find_first_of(' ');
     if(s.find("dir") == std::string::npos) {
-        tempFile.size = (long) std::stoi(s.substr(0, spacePos++));
+        tempFile.size = (unsigned long) std::stoi(s.substr(0, spacePos++));
         tempFile.name = s.substr(spacePos, s.size() - spacePos);
     } else {
         tempFile.size = -1;
@@ -79,67 +34,46 @@ file processItem(const std::string &s) {
     return tempFile;
 }
 
-void processLine(const std::string &s) {
-    // Two possible commands; ls and cd.
-    // Process them accordingly.
-    std::string n;
-    if(s.find("$ ls") != std::string::npos) {
-//        std::cout << s << std::endl;
-        std::string temp;
-        while(getline(input, temp)) {
-            if(temp.find("$ cd") != std::string::npos) {
-                processLine(temp);
-                break;
-            } else {
-                file toAdd = processItem(temp);
-                if(toAdd.size != -1) {
-                    tempDir->files.emplace_back(toAdd);
-                }
-            }
-        }
-    } else if(s.find("$ cd") != std::string::npos) {
-        if(tempDir != nullptr) {
-            // Finished processing files to be added
-            tempDir->processSize();
-            tempDir->processHash(count++);
-            directories.at(index) = *tempDir;
-        }
-//        std::cout << s << std::endl;
-        n = s.substr(5, s.size() - 5);
-        if(n == "..") {
-            tempDir = tempDir->parent;
-        } else {
-            if(tempDir != nullptr) {
-                prevDir = tempDir;
-            }
-            tempDir = new dir;
-            tempDir->name = n;
-            if(n != "/") {
-                tempDir->parent = prevDir;
-            }
-            directories.emplace_back(*tempDir);
-            index = directories.size() - 1;
-        }
-    }
-}
+std::vector<dir> directories;
 
-unsigned long calculateTotal(dir &d, int depth) {
-//    std::cout << d.name << " calculateTotal, depth = " << depth << std::endl;
-    unsigned long total = 0;
-    if(d.size <= 100000) {
-        total += d.size;
-    } else {
-        return -1;
-    }
-//    std::cout << d.name << " subDirs.size = " << d.subDirs.size() << std::endl;
-    for(auto &i: d.subDirs) {
-//        std::cout << i->subDirs.size() << std::endl;
-        unsigned long t = calculateTotal(i, ++depth);
-        if(t <= 100000) {
-            total += t;
+void createTree(const std::vector<std::string> &list) {
+    // Find the location in list of where the cd commands are.
+    // This can be used for parent references later.
+    std::vector<int> dirLocations;
+    for(int i = 0; i < list.size(); i++) {
+        if(list[i].find("$ cd") != std::string::npos && list[i].find("..") == std::string::npos) {
+            dirLocations.emplace_back(i);
         }
     }
-    return total;
+    // Construct the files from the previous locations.
+    for(auto i: dirLocations) {
+        dir newDir;
+        std::string name = list[i].substr(5, list[i].size() - 5);
+        newDir.name = name;
+        newDir.fileLine = i;
+        // Read files until it finds; a "$ cd" or the end of scope, and if j is beyond the scope of list.
+        for(int j = i + 2; list[j].find("$ cd") == std::string::npos && j < list.size(); j++) {
+            file toAdd = createFile(list[j]);
+            if(toAdd.size != -1) {
+                newDir.files.emplace_back(toAdd);
+            }
+        }
+        directories.emplace_back(newDir);
+    }
+    // Calculate the hash of the directory,
+    // The hash also needs the size of the directory.
+    for(int i = 0; i < directories.size(); i++) {
+        // Size calculation
+        unsigned long sizeTotal = 0;
+        for(const auto& j: directories[i].files) {
+            sizeTotal += j.size;
+        }
+        directories[i].size = sizeTotal;
+        // Hash calculation
+        std::size_t nameHash = std::hash<std::string>{}(directories[i].name);
+        std::size_t sizeHash = std::hash<unsigned long>{}(directories[i].size);
+        directories[i].hash = std::hash<std::size_t>{}(nameHash & (sizeHash) ^ (i << 1));
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -156,17 +90,16 @@ int main(int argc, char *argv[]) {
     if(!input.is_open()) {
         return -1;
     }
+    std::vector<std::string> inputList;
     std::string temp;
     while(getline(input, temp)) {
-        processLine(temp);                
+        inputList.emplace_back(temp);
     }
-    for(auto &i: directories) {
-        i.processSubdirs();
-    }
+
+    createTree(inputList);
+
     unsigned long total = 0;
-    for(auto i: directories) {
-        total += calculateTotal(i, 0);
-    }
+
     std::cout << std::endl << "Total = " << total << std::endl;
     return 0;
 }
