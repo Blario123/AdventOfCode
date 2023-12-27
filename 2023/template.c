456338ff77044a627a341904de01045284ef4e02
Blair Liversidge <19518532+Blario123@users.noreply.github.com>
2023-12-27 18:34:48 +0000

    Updated the template file
    The template now correctly add the includes and added the parseLine function too

--- 2023/template.c
+++ 2023/template.c
@@ -2,6 +2,17 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include <stdbool.h>
+
+char line[1000];
+size_t linePos = 0;
+
+void parseLine(const char* l, size_t length) {
+    // If the length of the line is 1, or the first character is a newline, return immediately as nothing is to be done.
+    if(length == 1 || l[0] == '\n') {
+        return;
+    }
+}
 
 int main(int argc, char** argv) {
     if(argc > 1) {
@@ -10,7 +21,16 @@ int main(int argc, char** argv) {
             printf("Could not open file.\n");
             return 1;
         }
-
+        char c = fgetc(input);
+        while(c != EOF) {
+            line[linePos++] = c;
+            if(c == '\n') {
+                parseLine(line, linePos);
+                memset(line, 0, 1000 * sizeof(char));
+                linePos = 0;
+            }
+            c = fgetc(input);
+        }
     } else {
         printf("No file given.\n");
         return 1;
