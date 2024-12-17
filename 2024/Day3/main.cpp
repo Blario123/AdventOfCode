a448c31ebdfefed5a5e8ba963ba3c83857109028
Blair Liversidge <19518532+Blario123@users.noreply.github.com>
2024-12-17 19:34:02 +0000

    Day 3

--- 2024/Day3/main.cpp
+++ 2024/Day3/main.cpp
@@ -1,3 +1,4 @@
+#include <cstdio>
 #include <fstream>
 #include <string>
 #include <vector>
@@ -5,14 +6,9 @@
 std::vector<std::string> contents;
 
 bool checkSubstring(const std::string &str) {
-    printf("Checking string \"%s\"\n", str.c_str());
-    if(str.find(' ') != std::string::npos) {
-        printf("Contains a space\n");
-        return false;
-    }
-    for(char i = 'a'; i < 'z'; i++) {
+    std::string toCheck = " abcdefghijklmnopqrstuvwxyz#~()[]{}!\"£$%^&*-_=+;:'@<.>/?\\|";
+    for(auto &i: toCheck) {
         if(str.find(i) != std::string::npos) {
-            printf("Contains illegal characters\n");
             return false;
         }
     }
@@ -23,9 +19,7 @@ bool checkSubstring(const std::string &str) {
         commaPos = str.find(',', commaPos + 1);
     }
     commaCount -= 1;
-    printf("%d\n", commaCount);
     if(commaCount != std::string::npos && commaCount > 1) {
-        printf("Too many commas\n");
         return false;
     }
     return true;
@@ -33,9 +27,9 @@ bool checkSubstring(const std::string &str) {
 
 int multiplySubstring(const std::string &str) {
     size_t sz = 0;
-    int n1 = std::stoi(str, &sz);
-    int n2 = std::stoi(str.substr(sz));
-    return n2 * n2;
+    int n1 = std::stoi(str.substr(0, str.find(",")), &sz);
+    int n2 = std::stoi(str.substr(sz + 1));
+    return n1 * n2;
 }
 
 int main(int argc, char** argv) {
@@ -51,21 +45,64 @@ int main(int argc, char** argv) {
             getline(input, line, '\n');
         }
         // Day 1 - Find and multiply
-        size_t sz = 0;
         int sum = 0;
         for(auto &i: contents) {
+            size_t sz = 0;
             size_t sz_ = 0;
             while(sz != std::string::npos) {
                 sz = i.find("mul(", sz_);
                 sz_ = sz + 4;
                 std::string mulStr = i.substr(sz_, i.find(")", sz_) - sz_);
                 if(checkSubstring(mulStr)) {
-                    // sum += multiplySubstring(mulStr);
-                } else {
-                    printf("Check failed\n");
+                    sum += multiplySubstring(mulStr);
                 }
             }
         }
+        printf("Corrupted memory = %d\n", sum);
+        // Day 2 - Find and multiply + do() and dont() actions
+        sum = 0;
+        bool perform = true;
+        for(auto &i: contents) {
+            size_t sz = 0;
+            size_t sz_ = 0;
+            while(sz != std::string::npos) {
+                int action = 1;
+                // Determine if mul, do or dont is closer
+                size_t mulPos = i.find("mul(", sz_);
+                size_t doPos = i.find("do()", sz_);
+                size_t dontPos = i.find("don't()", sz_);
+                size_t smallest = mulPos;
+                if(smallest > doPos) {
+                    action = 2;
+                    smallest = doPos;
+                }
+                if(smallest > dontPos) {
+                    action = 3;
+                    smallest = dontPos;
+                }
+                sz = smallest;
+                sz_ = sz + 4;
+                std::string mulStr;
+                switch(action) {
+                    case 1: // mul
+                        if(perform) {
+                            mulStr = i.substr(sz_, i.find(")", sz_) - sz_);
+                            if(checkSubstring(mulStr)) {
+                                sum += multiplySubstring(mulStr);
+                            }
+                        }
+                        break;
+                    case 2: // do
+                        perform = true;
+                        break;
+                    case 3: // dont
+                        perform = false;
+                        sz_ += 3;
+                        break;
+                }
+            }
+        }
+        printf("Filtered corrupted memory = %d\n", sum);
     }
     return 0;
 }
