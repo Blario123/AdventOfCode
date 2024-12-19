fb857d1315b132906857dd49d7e07c9d528cd453
Blair Liversidge <19518532+Blario123@users.noreply.github.com>
2024-12-19 21:32:43 +0000

    Day 4

--- 2024/Day4/main.cpp
+++ 2024/Day4/main.cpp
@@ -6,8 +6,9 @@
 
 std::vector<std::string> wordSearch;
 std::vector<std::pair<int,int>> xPositions;
+std::vector<std::pair<int,int>> aPositions;
 
-int checkWord(const std::pair<int,int> &p) {
+int checkXMAS(const std::pair<int,int> &p) {
     int count = 0;
     std::string wordToCheck;
     if(p.first < wordSearch[0].length() - 3) {
@@ -60,15 +61,36 @@ int checkWord(const std::pair<int,int> &p) {
             wordToCheck[i] = wordSearch[p.second + i][p.first];
         }
         if(wordToCheck == "XMAS") {
-				count++;
+            count++;
         }
     }
-    if(p.second > 3) {
+    if(p.second >= 3) {
         for(int i = 0; i < 4; i++) {
             wordToCheck[i] = wordSearch[p.second - i][p.first];
         }
         if(wordToCheck == "XMAS") {
-				count++;
+            count++;
+        }
+    }
+    return count;
+}
+
+
+int checkMAS(const std::pair<int,int> &p) {
+    int count = 0;
+    std::string wordToCheck;
+    if(p.first > 0 && p.first < (wordSearch[0].size() - 1) && p.second > 0 && p.second < (wordSearch.size() - 1)) {
+        wordToCheck = "...";
+        for(int i = 0; i < 3; i++) {
+            wordToCheck[i] = wordSearch[p.second + (i - 1)][p.first + (i - 1)];
+        }
+        if(wordToCheck == "MAS" || wordToCheck == "SAM") {
+            for(int i = 0; i < 3; i++) {
+                wordToCheck[i] = wordSearch[p.second + (i - 1)][p.first - (i - 1)];
+            }
+            if(wordToCheck == "MAS" || wordToCheck == "SAM") {
+                count++;
+            }
         }
     }
     return count;
@@ -84,21 +106,36 @@ int main(int argc, char** argv) {
         getline(input, line, '\n');
         while(input.good()) {
             wordSearch.emplace_back(line);
-            size_t sz = 0;
-            while(sz != std::string::npos) {
-                sz = line.find('X', sz);
-                if(sz != std::string::npos) {
-                    xPositions.emplace_back(std::pair<int,int>(sz, wordSearch.size() - 1));
-                    sz++;
+            size_t szX = 0;
+            while(szX != std::string::npos) {
+                szX = line.find('X', szX);
+                if(szX != std::string::npos) {
+                    xPositions.emplace_back(std::pair<int,int>(szX, wordSearch.size() - 1));
+                    szX++;
+                }
+            }
+            size_t szA = 0;
+            while(szA != std::string::npos) {
+                szA = line.find('A', szA);
+                if(szA != std::string::npos) {
+                    aPositions.emplace_back(std::pair<int,int>(szA, wordSearch.size() - 1));
+                    szA++;
                 }
             }
             getline(input, line, '\n');
         }
         int sum = 0;
-        for(auto &i: xPositions) {
-            sum += checkWord(i);
+        // Day 1 - Finding all XMASs
+        for(const auto &i: xPositions) {
+            sum += checkXMAS(i);
         }
         printf("XMAS count = %d\n", sum);
+        // Day 2 - Finding all MAS in a X pattern
+        sum = 0;
+        for(const auto &i: aPositions) {
+            sum += checkMAS(i);
+        }
+        printf("X-MAS count = %d\n", sum);
     }
     return 0;
 }
