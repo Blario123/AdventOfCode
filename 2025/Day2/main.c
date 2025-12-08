a8c8acc2c05bdcd26575840950ef2277d94a50d9
Blario123 <19518532+Blario123@users.noreply.github.com>
2025-12-08 10:58:34 +0000

    Day 2 -- Part 1

--- 2025/Day2/main.c
+++ 2025/Day2/main.c
@@ -3,20 +3,22 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
-//  1010329272 -- LOW
-//  5305296568 -- LOW
+
 void substr(char* buf, char* start, char* end) {
     // Zero the buffer
     memset(buf, 0x0, 1024);
+    // Create another string pointer to the same start point
     char* _start = start;
+    // If the strchr value is NULL, fin the last digit of the original buffer
     if(end == NULL) {
-        // Find the end from the start
         while(*start != '\0') {
             start++;
         }
+        // Decrement to remove the null-end
         end = start - 1;
     }
     int index = 0;
+    // Form the sub-string buffer from the start to the end point
     while(_start != end) {
         if(*_start != ',') {
             buf[index++] = *_start;
@@ -25,16 +27,25 @@ void substr(char* buf, char* start, char* end) {
     }
 }
 
-bool isPalindromic(int pal) {
+bool isRepeat(unsigned long long pal) {
+    // Create a local buffer
     char str[1024];
-    sprintf(str, "%d", pal);
+    // Convert the ull input to c-string
+    sprintf(str, "%llu", pal);
+    // If the string is an odd length, ignore as cannot be invalid
     if((strlen(str) % 2) != 0) {
         return false;
     }
+    // Create a second local buffer, half the size of the first ull buffer.
     char str_half[512];
+    // Zero to ensure no erroneous characters
+    memset(str_half, 0, 512);
+    // Take the first half of the string
     for(int i = 0; i < (strlen(str) / 2); i++) {
         str_half[i] = str[i];
     }
+    // Compare the first half, to the second
+    // Add the halfway point to the pointer to start the second half without copying
     if(strcmp(str_half, str + (strlen(str) / 2)) == 0) {
         return true;
     }
@@ -42,15 +53,16 @@ bool isPalindromic(int pal) {
 }
 
 unsigned long long invalidId(char* buf) {
+    // To correctly handle the input, all values must be ull.
     unsigned long long sum = 0;
-    int from = 0;
-    int to = 0;
+    unsigned long long from = 0;
+    unsigned long long to = 0;
     int index = 0;
     char num[strlen(buf)];
     memset(num, 0, strlen(buf));
     while(*buf != '\0') {
         if(*buf == '-') {
-            from = atoi(num);
+            from = atoll(num);
             memset(num, 0, strlen(buf));
             index = 0;
         } else {
@@ -58,13 +70,13 @@ unsigned long long invalidId(char* buf) {
         }
         buf++;
     }
-    to = atoi(num);
-    for(int i = from; i <= to; i++) {
-        if(isPalindromic(i)) {
+    to = atoll(num);
+    // Iterate within the range of the values
+    for(unsigned long long i = from; i <= to; i++) {
+        if(isRepeat(i)) {
             sum += i;
         }
     }
-    printf("from = %d, to = %d\n", from, to);
     return sum;
 }
 
@@ -87,7 +99,7 @@ int main(int argc, char** argv) {
                 sum += invalidId(buf);
 			}
         }
-        printf("%llu\n", sum);
+        printf("Sum of invalid IDs = %llu\n", sum);
         fclose(p_file);
     } else {
         printf("input filename required.\n");
